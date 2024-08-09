58f3777cb2121d8f1b9c0dd0aae7a120b5181272
Tim Pilius <tpilius@gmail.com>
2024-08-09 16:45:48 -0400

    Disable comparison util

--- RiotPrefill/Program.cs
+++ RiotPrefill/Program.cs
@@ -6,7 +6,7 @@
 
         public static async Task Main()
         {
-            AppConfig.CompareAgainstRealRequests = true;
+            //AppConfig.CompareAgainstRealRequests = true;
 
             // Downloading manifest
             var parseTimer = Stopwatch.StartNew();
